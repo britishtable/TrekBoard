eea7051e515c948cbf838546b0ad371f89f08281
britishtable <keeganjoubert22@gmail.com>
2026-07-01 21:35:43 +0200

    fix: mock maplibre-gl in AppShell tests to avoid jsdom createObjectURL crash

--- src/components/AppShell.test.tsx
+++ src/components/AppShell.test.tsx
@@ -1,5 +1,55 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
+
+vi.mock('maplibre-gl', () => {
+  class Map {
+    on() {
+      return this;
+    }
+    remove() {}
+    setCenter() {
+      return this;
+    }
+    getBounds() {
+      return {
+        getSouth: () => 0,
+        getWest: () => 0,
+        getNorth: () => 0,
+        getEast: () => 0,
+      };
+    }
+    queryRenderedFeatures() {
+      return [];
+    }
+  }
+  class Marker {
+    setLngLat() {
+      return this;
+    }
+    setPopup() {
+      return this;
+    }
+    addTo() {
+      return this;
+    }
+    remove() {}
+  }
+  class Popup {
+    setLngLat() {
+      return this;
+    }
+    setDOMContent() {
+      return this;
+    }
+    addTo() {
+      return this;
+    }
+    remove() {}
+  }
+  return { default: { Map, Marker, Popup } };
+});
+
 import AppShell from './AppShell';
 
 test('shows empty state and can create the first trip', async () => {
